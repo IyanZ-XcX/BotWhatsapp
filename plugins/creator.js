11a71895d5532f8199787e9f881cb50d781f6c5e
Iyann <101798961+IyanZ-XcX@users.noreply.github.com>
2022-04-04 11:04:45 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,150 +1,19 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-  let name = 'ᴛʜᴇ.ꜱᴀᴅ.ʙᴏʏ01' //ubah jadi namamu
-  number = owner[0].replace(/[^0-9]/g, '')
-  let njid = number + '@s.whatsapp.net'
-  let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
-
-  let name2 = 'Bochil Gaming' //ubah jadi namamu
-  number2 = owner[1].replace(/[^0-9]/g, '')
-  let njid2 = number2 + '@s.whatsapp.net'
-  let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name3 = 'Kanna Bot' //ubah jadi namamu
-  number3 = owner[2].replace(/[^0-9]/g, '')
-  let njid3 = number3 + '@s.whatsapp.net'
-  let onW3 = await this.isOnWhatsApp(njid3) || { isBusiness: false }
-  
-  let name4 = 'ᵀᵒʰʳᵘ ᴮᴼᵀᶻ' //ubah jadi namamu
-  number4 = owner[3].replace(/[^0-9]/g, '')
-  let njid4 = number4 + '@s.whatsapp.net'
-  let onW4 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name5 = 'Nurutomo' //ubah jadi namamu
-  number5 = owner[4].replace(/[^0-9]/g, '')
-  let njid5 = number5 + '@s.whatsapp.net'
-  let onW5 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name6 = 'ariffb' //ubah jadi namamu
-  number6 = owner[5].replace(/[^0-9]/g, '')
-  let njid6 = number6 + '@s.whatsapp.net'
-  let onW6 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name7 = 'Adi-official' //ubah jadi namamu
-  number7 = owner[6].replace(/[^0-9]/g, '')
-  let njid7 = number7 + '@s.whatsapp.net'
-  let onW7 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  let name8 = 'Diablo BOTz' //ubah jadi namamu
-  number8 = owner[7].replace(/[^0-9]/g, '')
-  let njid8 = number8 + '@s.whatsapp.net'
-  let onW8 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
-  
-  this.sendMessage(m.chat, {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  await conn.sendMessage(m.chat, {
     contacts: [{
-      displayname: name, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name.replace(/\n/g, '\\n')};;;
-FN:${name.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}${onW.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid].vname || this.getName(njid)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name2, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name2.replace(/\n/g, '\\n')};;;
-FN:${name2.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number2}:${PhoneNumber('+' + number2).getNumber('international')}${onW2.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid2].vname || this.getName(njid2)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid2)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name3, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name3.replace(/\n/g, '\\n')};;;
-FN:${name3.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number3}:${PhoneNumber('+' + number3).getNumber('international')}${onW3.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid3].vname || this.getName(njid3)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid3)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name4, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name4.replace(/\n/g, '\\n')};;;
-FN:${name4.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number4}:${PhoneNumber('+' + number4).getNumber('international')}${onW4.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid4].vname || this.getName(njid4)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid4)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name5, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name5.replace(/\n/g, '\\n')};;;
-FN:${name5.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number5}:${PhoneNumber('+' + number5).getNumber('international')}${onW5.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid5].vname || this.getName(njid5)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid5)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name6, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name6.replace(/\n/g, '\\n')};;;
-FN:${name6.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number6}:${PhoneNumber('+' + number6).getNumber('international')}${onW6.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid6].vname || this.getName(njid6)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid6)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name7, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name7.replace(/\n/g, '\\n')};;;
-FN:${name7.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number7}:${PhoneNumber('+' + number7).getNumber('international')}${onW7.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid7].vname || this.getName(njid7)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid7)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
-    }, {
-      displayname: name8, vcard: `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name8.replace(/\n/g, '\\n')};;;
-FN:${name8.replace(/\n/g, '\\n')}
-TEL;type=CELL;type=VOICE;waid=${number8}:${PhoneNumber('+' + number7).getNumber('international')}${onW8.isBusiness ? `
-X-WA-BIZ-NAME:${(this.contacts[njid8].vname || this.getName(njid8)).replace(/\n/, '\\n')}
-X-WA-BIZ-DESCRIPTION:${((await this.getBusinessProfile(njid8)).description || '').replace(/\n/g, '\\n')}
-` : ''}
-END:VCARD
-`.trim()
+    "displayName": "Iyann Ganteng",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Iyann Ganteng\nitem1.TEL;waid=6285742344873:6285742344873\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:kastaraiyan@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/IyanZ-XcX\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ Tohka Yatogami ]───────\nEND:VCARD"
+  }, {
+    "displayName": "Whatsapp",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
-  conn.send2Button(m.chat, `Mau tanya apa ke owner ku ?\nsc bot ? tekan button bawah ini jika tidak ada button ketik *.sc*`, 'Tuh owner Ku Jangan Galak² Ya >_<, Nanti Ku Banned Nanges', 'Youtube owner', '.ytowner', 'Source code', '.sc', m)
-  }
+}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner)$/i
 
 module.exports = handler
